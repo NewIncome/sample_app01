5ba84fdaa7a9962283cebd876597020463dce74a
NewIncome <alfredo_cardenas@live.com>
2020-04-07 12:54:37 -0500

    Add some comments and a button in the home.html to get ready for bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
   # get 'static_pages/home' # instead will use root
+  # can also be done with, root to: 'static_pages#home'
+  root 'static_pages#home'
   get '/about', to:'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
