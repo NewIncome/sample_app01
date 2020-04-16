fd6e8b71d3c7467162447d3d67d06341934623e3
NewIncome <alfredo_cardenas@live.com>
2020-04-16 13:08:32 -0500

    Add before_save validation to downcase a new email saved to the db.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { in: 6..25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
