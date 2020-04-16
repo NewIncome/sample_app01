0749c6f64c768d1b08a57b3236c3c1bdeac365c1
NewIncome <alfredo_cardenas@live.com>
2020-04-16 17:11:59 -0500

    Add form and bootstrap & css styling; Add the newUser_button; Edit the routes. Users#controller yet to be finished.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
   end
 
   private
 
-  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   # get 'static_pages/home' # instead will use root
   # can also be done with, root to: 'static_pages#home'
   root 'static_pages#home'
   get '/about', to:'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  resources :users#, only: [:show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
