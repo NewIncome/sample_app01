0f774406636bdb3a9fcd32c4fb91a896cb52bc30
NewIncome <alfredo_cardenas@live.com>
2020-04-16 15:31:18 -0500

    Create the gravatar method and call it in the view.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  # Fravatar URLs are based on MD5 Hash of the users emailt.binary :
+  # We implement it in Ruby using 'hexdigest' from the Digest lib.
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt:user.name, class: "gravatar d-block", size:100)
+  end
 end
