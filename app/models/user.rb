655799cdbf18490c54d376dee787b51c0b7c6555
NewIncome <alfredo_cardenas@live.com>
2020-04-22 13:31:05 -0500

    1- Add a new migration to add a new column, remember_digest, to the Users table. This to be able to have a permament cookie and validate it for availability of permanent sessions. 2- Added 5 methods to the User_model for permanent session availability: - 1)User.new_token - 2)User.digest(token) - 3)remember - 4)authenticated?(rem_token) - 5)forget

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,34 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
+
+  # create a random new token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # creates a safe_hashed version of a string/token
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Remembers a user in the DB by the remember_digest for persistent sessions
+  def remember
+    remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given remember_token matches the digest.
+  def authenticated?(rem_token)
+    return false if remember_digest.nil? # this is to not even test if it doesn't exist
+    BCrypt::Password.new(remember_digest).is_password?(rem_token)
+  end
+
+  # Forget a remember_digest to delete a permamnent session
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
