41ad767eef16bab6fa59ec56cc234d6a3bb7ed88
NewIncome <alfredo_cardenas@live.com>
2020-04-22 16:05:18 -0500

    Add some comments for the new methods for the remember_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token # 6)to have r/w access to the remember_token
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { in: 6..25 }
@@ -7,31 +8,35 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  # create a random new token
+  # The following (6) methods are needed to be able to manipulate
+  # a permament session with a permanent cookie via a the remember_digest
+  # new column in the Users table.
+
+  # 1) create a random new token
   def User.new_token
     SecureRandom.urlsafe_base64
   end
 
-  # creates a safe_hashed version of a string/token
+  # 2) creates a safe_hashed version of a string/token
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  # Remembers a user in the DB by the remember_digest for persistent sessions
+  # 3) Remembers a user in the DB by the remember_digest for persistent sessions
   def remember
     remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # Returns true if the given remember_token matches the digest.
+  # 4) Returns true if the given remember_token matches the digest.
   def authenticated?(rem_token)
     return false if remember_digest.nil? # this is to not even test if it doesn't exist
     BCrypt::Password.new(remember_digest).is_password?(rem_token)
   end
 
-  # Forget a remember_digest to delete a permamnent session
+  # 5) Forget a remember_digest to delete a permamnent session
   def forget
     update_attribute(:remember_digest, nil)
   end
