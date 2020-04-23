b12db13e27213be7fec47ab7480c836225b65075
NewIncome <alfredo_cardenas@live.com>
2020-04-23 18:02:21 -0500

    Add Authorization or log_in Requirement before_access feature: 1- Create the logged_in_user method to restrict access to edit, update, index and destroy actions/methods to only logged_in users. 2- Call that function before accessing those methods with the before_action method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 
   def show
     @user = User.find(params[:id])
@@ -37,6 +38,19 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # The following methods are needed to REQUIRE that only the correct logged_in
+  # user is the one to do certain actions, and if not, do not view those pages
+  # and be sent to another link/page/action. And are private so no-one can manipulate
+  # them.
+  
+  # To check if it is a logged_in_user before accessing index, edit, update & destroy
+  def logged_in_user
+    unless logged_in?   # if not logged_in be sent to login_url with a flash msg.
+      flash[:danger] = "Please log in to have access."
+      redirect_to login_url
+    end
   end
 
 end
