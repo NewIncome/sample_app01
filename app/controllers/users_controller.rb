899a5c643e901d712921a7a93b8506d7ce0a685e
NewIncome <alfredo_cardenas@live.com>
2020-04-24 13:47:36 -0500

    Add methods to not only let the current_user his own data and if trying to access another users edit_page redirect them to that users page: - Add the method current_user to do the check and the redirect; - Add the before_action with that method so it is enforced when trying to access the edit and update methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -51,6 +52,12 @@ class UsersController < ApplicationController
       flash[:danger] = "Please log in to have access."
       redirect_to login_url
     end
+  end
+
+  # M.to restrict info_update availability to only that logged_in user's info.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(@user) unless @user == current_user
   end
 
 end
