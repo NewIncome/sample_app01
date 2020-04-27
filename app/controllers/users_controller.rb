425ab3bdef1dfc7974f80e4edd2f4f3f65d36e44
NewIncome <alfredo_cardenas@live.com>
2020-04-27 16:36:38 -0500

    Add the All-users-view: 1- Create the index file with the code to view all the users paginated; 2- Create the _user partial to help and add view to each paginated user; 3- Add the user#index method to call index and send the paginated users in the @users variable; 4- Add the route for the user#index; 5- Add a link to users#index view in the _header; 6- Add some css for better view of the pagination menu.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
 
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users, only: [:show, :create, :edit, :update]
+  resources :users, only: [:index, :show, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
